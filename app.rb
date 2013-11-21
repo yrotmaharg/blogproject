2e7cce3726f66eb3d146051558b46c181ed85ee9
Victoria <victoriaggraham@gmail.com>
2013-11-21 13:03:04 -0500

    second attempt at pushing initial code

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'haml'
+require 'sinatra/activerecord'
+
+configure(:development){ set :database, "sqlite3:///twitterclone.sqlite3" }
+#set is a method,so don't forget a space (or parenthesis)
+
+require 'bundler/setup'
+require 'sinatra/base'
+require 'rack-flash'
+require 'rake'
+
+require './models'
